9eebf2cae078fd0340702f43fd90db733721ee73
OldNGrey <73047349+OldNGreyBMR@users.noreply.github.com>
2022-10-16 09:24:29 +1000

    zc157d zc158 PHP 8.0 to 8.2
    lang file for zc158

---
+++ files/admin/includes/languages/english/extra_definitions/lang.back_in_stock_notifications.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Ceon Back In Stock Notifications Admin Extra Language Definitions.
+ *
+ * @package     ceon_back_in_stock_notifications
+ * @author      Conor Kerr <zen-cart.back-in-stock-notifications@dev.ceon.net>
+ * @copyright   Copyright 2004-2012 Ceon
+ * @copyright   Portions Copyright 2003-2006 Zen Cart Development Team
+ * @copyright   Portions Copyright 2003 osCommerce
+ * @license     http://www.gnu.org/copyleft/gpl.html   GNU Public License V2.0
+ * @version     $Id: back_in_stock_notifications.php 936 2022-10-14  BMH $
+ */
+// BMH 2022-10-14
+$define =[
+    'BOX_CEON_BACK_IN_STOCK_NOTIFICATIONS' => 'Ceon Back In Stock Notifications',
+    'BOX_CEON_BACK_IN_STOCK_NOTIFICATIONS_CONFIG_GROUP' => 'Ceon Back In Stock Notifications', 
+    'TABLE_HEADING_PRODUCT_MODEL' => 'Product Model', // BMH
+    ];
+return $define;
+
+?>
